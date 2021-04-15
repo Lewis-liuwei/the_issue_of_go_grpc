a25030fd3f098c60b4e47404a58cef976198dcf4
Lewis-Liuwei <lewis-liuwei@outlook.com>
2021-04-15 18:14:32 +0800

    new a laptop sample done

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"go_grpc/sample"
 )
 
 /**
@@ -11,5 +12,6 @@ proto/*.proto 该文件夹内的文件
 --gofast_out=plugins=grpc:路径 将协议缓冲区代码转译后存储的路径设置
 */
 func main() {
-	fmt.Println("hello go-grpc")
+	kb := sample.NewKeyboard()
+	fmt.Printf("%v\n", kb)
 }
--- sample/generator.go
+++ sample/generator.go
@@ -1,1 +1,114 @@
 package sample
+
+import (
+	"go_grpc/pb"
+
+	"github.com/golang/protobuf/ptypes"
+)
+
+func NewKeyboard() *pb.Keyboard {
+	kb := &pb.Keyboard{
+		Layout:   randomKeyboardLayout(),
+		Blacklit: randomBool(),
+	}
+	return kb
+}
+
+func NewCPU() *pb.CPU {
+	brand := randomCPUBrand()
+	name := randomCPUName(brand)
+	numberCores := randomInt(2, 8)
+	numberThreds := randomInt(numberCores, 12)
+	minHz := randomFloat64(2.0, 3.0)
+	maxHz := randomFloat64(minHz, 5.0)
+	cpu := &pb.CPU{
+		Brand:         brand,
+		Name:          name,
+		NumberCores:   uint32(numberCores),
+		NumberThreads: uint32(numberThreds),
+		MinGhz:        minHz,
+		MaxGhz:        maxHz,
+	}
+	return cpu
+}
+
+func NewGPU() *pb.GPU {
+	brand := randomGPUBrand()
+	name := randomGPUName(brand)
+	minHz := randomFloat64(1.0, 1.5)
+	maxHz := randomFloat64(minHz, 2.0)
+	memory := &pb.Memory{
+		Value: uint64(randomInt(2, 6)), // 内存值
+		Unit:  pb.Memory_GIGABYTE,      // 单位
+	}
+	gpu := &pb.GPU{
+		Brand:  brand,
+		Name:   name,
+		MinGhz: minHz,
+		MaxGhz: maxHz,
+		Memory: memory,
+	}
+
+	return gpu
+}
+
+func NewRAM() *pb.Memory {
+	return &pb.Memory{
+		Value: uint64(randomInt(4, 64)),
+		Unit:  pb.Memory_GIGABYTE,
+	}
+}
+
+func NewSSD() *pb.Storage {
+	return &pb.Storage{
+		Driver: pb.Storage_SSD,
+		Memory: &pb.Memory{
+			Value: uint64(randomInt(128, 1024)),
+			Unit:  pb.Memory_GIGABYTE,
+		},
+	}
+}
+
+func NewHDD() *pb.Storage {
+	return &pb.Storage{
+		Driver: pb.Storage_HDD,
+		Memory: &pb.Memory{
+			Value: uint64(randomInt(1, 6)),
+			Unit:  pb.Memory_TERABYTE,
+		},
+	}
+}
+
+func NewScreen() *pb.Screen {
+	return &pb.Screen{
+		SizeInch:   randomFloat32(13, 17),
+		Resolution: randomScreenResolution(),
+		Panel:      randomPanel(),
+		Multitouch: randomBool(),
+	}
+}
+
+// new a laptop sample
+func NewLaptop() *pb.Laptop {
+	brand := randomLaptopBrand()
+	name := randomLaptopName(brand)
+	laptop := &pb.Laptop{
+		Id:       randomID(),
+		Brand:    brand,
+		Name:     name,
+		Cpu:      NewCPU(),
+		Ram:      NewRAM(),
+		Gpus:     []*pb.GPU{NewGPU()},
+		Storages: []*pb.Storage{NewSSD(), NewHDD()},
+		Screen:   NewScreen(),
+		Keyboard: NewKeyboard(),
+		Weight: &pb.Laptop_WeightKg{
+			WeightKg: randomFloat64(1.0, 3.0),
+		},
+		PriceUsd:    randomFloat64(1500, 3000),
+		ReleaseYear: uint32(randomInt(2015, 2019)),
+		UpdatedAt:   ptypes.TimestampNow(),
+	}
+
+	return laptop
+}

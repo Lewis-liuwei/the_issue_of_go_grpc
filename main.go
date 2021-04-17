5f39abcebafeafcc5cdae78de064bc955098db45
Lewis-Liuwei <lewis-liuwei@outlook.com>
2021-04-17 15:19:07 +0800

    created some files

--- main.go
+++ main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"go_grpc/sample"
+	"runtime"
 )
 
 /**
@@ -12,6 +12,10 @@ proto/*.proto 该文件夹内的文件
 --gofast_out=plugins=grpc:路径 将协议缓冲区代码转译后存储的路径设置
 */
 func main() {
-	laptop := sample.NewLaptop()
-	fmt.Printf("%v\n", laptop)
+	// laptop := sample.NewLaptop()
+	// fmt.Printf("%v\n", laptop)
+	fmt.Println("cpus:", runtime.NumCPU())
+	fmt.Println("goroot:", runtime.GOROOT())
+	fmt.Println("archive:", runtime.GOOS)
+
 }
